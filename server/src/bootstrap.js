ad8c60a1fdabbb8942a7876ad6bc12452e00ab55
Leandro Rocha <prog.leandro@gmail.com>
2019-11-03 22:49:50 -0300

    Teste de criação de usuário

--- server/src/bootstrap.js
+++ server/src/bootstrap.js
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config({
+// console.log(process.env);
+require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
